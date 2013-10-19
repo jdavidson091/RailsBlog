d16acb413df4bde70b84a0ea0b555a05ec0283f4
John Davidson <jdavidson091@gmail.com>
2013-10-18 20:43:21 -0500

    Added sign in/out functionality for sessions and users

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -4,23 +4,22 @@ describe "Authentication" do
 
   subject { page }
 
-  describe "with valid information" do
-    let(:user) { FactoryGirl.create(:user) }
-    before do
-      fill_in "Email",    with: user.email.upcase
-      fill_in "Password", with: user.password
-      click_button "Sign in"
-    end
-
-    it { should have_title(user.name) }
-    it { should have_link('Profile',     href: user_path(user)) }
-    it { should have_link('Sign out',    href: signout_path) }
-    it { should_not have_link('Sign in', href: signin_path) }
-  end
-
   describe "signin" do
 
     before { visit signin_path }
+
+    describe "with valid information" do
+      let(:user) { FactoryGirl.create(:user) }
+      before do
+        fill_in "Email",    with: user.email.upcase
+        fill_in "Password", with: user.password
+        click_button "Sign in"
+      end
+
+      it { should have_title(user.name) }     ##
+      it { should have_link('Profile',     href: user_path(user)) }  ##
+      it { should have_link('Sign out',    href: signout_path) }     ##
+    end
 
     describe "with invalid information" do
       before { click_button "Sign in" }
@@ -31,6 +30,11 @@ describe "Authentication" do
       describe "after visiting another page" do
         before { click_link "Home" }
         it { should_not have_selector('div.alert.alert-error') }
+      end
+
+      describe "followed by signout" do
+        before { click_link "Sign out" }
+        it { should have_link('Sign in') }    ###
       end
     end
   end
